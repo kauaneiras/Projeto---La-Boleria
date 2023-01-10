7b23c188f8817097f5ef300d878041ec3347be99
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2023-01-10 18:58:14 -0300

    finish

--- src/schemas/cackes.schema.js
+++ src/schemas/cackes.schema.js
@@ -1,9 +1,9 @@
 import joi from 'joi';
 
 const cakeSchema = joi.object({
-    //description pode ser vazia
+    //price deve ser maior que 0
     name: joi.string().min(2).required(),
-    price: joi.number().min(0).required(),
+    price: joi.number().min(0.1).required(),
     description: joi.string(),
     image: joi.string().uri().required()
 });
